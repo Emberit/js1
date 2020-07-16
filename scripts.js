0c1ece7febcafc2c15c33f144460eed808527203
Emberit <3039216@gmail.com>
2020-07-16 16:52:38 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,112 @@
+let a1 = 5 % 3;
+let a2 = 3 % 5;
+let a3 = 5 + '3';
+let a4 = '5' - 3;
+let a5 = 75 + 'кг';
+let a6 = 785 * 653; 
+let a7 = 100 / 25; 
+let a8 = 0 * 0;
+let a9 = 0 / 2; 
+let a10 = 89 / 0; 
+let a11 = 98 + 2; 
+let a12 = 5 - 98;
+let a13 = (8 + 56 * 4) / 5;
+let a14 = (9 - 12) * 7 / (5 + 2);
+let a15 = +"123"; 
+let a16 = 1 || 0; 
+let a17 = false || true;
+let a18 = true > 0;
+
+console.log('Задание 2:');
+console.log(a1);
+console.log(typeof a1);
+console.log(a2);
+console.log(typeof a2);
+console.log(a3);
+console.log(typeof a3);
+console.log(a4);
+console.log(typeof a4);
+console.log(a5);
+console.log(typeof a5);
+console.log(a6);
+console.log(typeof a6);
+console.log(a7);
+console.log(typeof a7);
+console.log(a8);
+console.log(typeof a8);
+console.log(a9);
+console.log(typeof a9);
+console.log(a10);
+console.log(typeof a10);
+console.log(a11);
+console.log(typeof a11);
+console.log(a12);
+console.log(typeof a12);
+console.log(a13);
+console.log(typeof a13);
+console.log(a14);
+console.log(typeof a14);
+console.log(a15);
+console.log(typeof a15);
+console.log(a16);
+console.log(typeof a16);
+console.log(a17);
+console.log(typeof a17);
+console.log(a18);
+console.log(typeof a18);
+
+console.log('Задание 3:');
+let width = 10;
+let height = 23;
+let SPryam = width * height;
+
+console.log(SPryam + ' см');
+
+console.log('Задание 4:');
+let height_cil = 10;
+let diam_osnovania = a7;
+let VCilindra = diam_osnovania * height_cil;
+
+console.log(VCilindra + ' м');
+
+console.log('Задание 5:');
+let radius_kruga = 5;
+let pi = 3.14;
+let SKruga = radius_kruga * radius_kruga * pi;
+
+console.log(SKruga + ' см');
+
+console.log('Задание 6:');
+let a = 5,
+    b = 7,
+    h = 10,
+    STrap = h / 2 * a * b;
+
+console.log(STrap + ' см');
+
+console.log('Задание 7:');
+let S = 2000000,
+    p = 0.1,
+    years = 5,
+    Pereplata = S * p * 5;
+
+console.log(Pereplata + ' руб');
+
+console.log('Задание 9:');
+
+console.log('Бывало, спит у ног собака,\nкостер занявшийся гудит,\nи женщина из полумрака\nглазами зыбкими глядит.\n\nПотом под пихтою приляжет\nна куртку рыжую мою.\nи мне, задумчивая, скажет:\n\n"А ну-ка, спой!.."- и я пою. ');
+
+console.log('Задание 10:');
+
+let zadan1 = ' индо земля зашаталась под ногами-и вырос,';
+let zadan2 = ' и заревел он голосом диким…'
+let zadan3 = ' блеснула молния и ударил гром,'
+let zadan4 = ' а так какое-то чудище, страшное и мохнатое,';
+let zadan5 = ' как будто из-под земли, перед купцом:'
+let zadan6 = 'Он подошёл и сорвал аленький цветочек.'
+let zadan7 = ' зверь не зверь, человек не человек,';
+let zadan8 = ' В ту же минуту, безо всяких туч,'
+
+console.log (zadan6 + zadan8 + zadan3 + zadan1 + zadan5 + zadan7 + zadan4 + zadan2)
+
+
---
+++ scripts2.js
@@ -0,0 +1,12 @@
+
+console.log('Задание 8:');
+
+a = 8;
+b = 3;
+let x1 = 8 - a / 2 + b;
+let x2 = (a - 15 * b) / (b - 6); 
+let x3 = 23780 / (a + b + 3);
+
+console.log ('x = ' + x1);
+console.log('x = ' + x2);
+console.log('x = ' + x3);
